54a0b7f9f07317ff52bf1f784ebee9a39406a05f
DataChanger <cyberkhan@mail.ru>
2022-05-29 04:46:33 +0500

    changes

---
+++ 7/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"; "net/http"; "html/template"
+)
+type Gym struct{
+	slave_name string
+	age uint8
+	cock float32 
+}
+func (g Gym) Getallinfo() string{
+	return fmt.Sprintf("Name of slave %s his age %d and most important his cock is %f",g.slave_name, g.age,g.cock)
+}
+func (g *Gym) Change_name(Newname string) {
+	g.slave_name = Newname
+}
+func home_page(w http.ResponseWriter, r *http.Request){
+	Master := Gym {}
+	tmpl,_ := template.ParseFiles("templates/site2.html")
+	tmpl.Execute(w,Master)
+}
+func contact_page(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"awsome balls")
+}
+
+func HandleRequest(){
+	http.HandleFunc("/",home_page)
+	http.HandleFunc("/contacts/",contact_page)
+	http.ListenAndServe(":8080",nil)
+
+}
+func main() {
+	HandleRequest()
+}
